ddd51ae066d340ec635355a1b9c46ae27cab993c
Benjamin Chaminade <benjamin@trendemic.net>
2014-07-08 15:14:58 +0200

    exercice 4

--- 01-Ruby/01-Programming-basics/04-What-your-name/lib/compute_name.rb
+++ 01-Ruby/01-Programming-basics/04-What-your-name/lib/compute_name.rb
@@ -1,6 +1,4 @@
 def compute_name(first_name, middle_name, last_name)
-  name =
-  puts "Bonjour #{first_name} #{middle_name} #{last_name}".capitalize
-
+  "#{first_name} #{middle_name} #{last_name}"
   # TODO: return full name using string interpolation
 end
--- 01-Ruby/01-Programming-basics/04-What-your-name/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/04-What-your-name/lib/interface.rb
@@ -1,6 +1,8 @@
 require_relative "compute_name"
 
 def ask_and_get(param)
+ puts "Enter your #{param}"
+return gets.chomp()
   #TODO:
   #- Ask the user to write a value for param
   #- Get and return the string inputted by the user
@@ -8,17 +10,18 @@ end
 
 def name_from_terminal
   first_name = ask_and_get("first name")
-  second_name = ask_and_get("middle name")
+  middle_name = ask_and_get("second name")
   last_name = ask_and_get("last name")
 
-  compute_name(first_name, second_name, last_name)
+  compute_name(first_name, middle_name, last_name)
 end
 
 full_name = name_from_terminal
 
-full_name = "first_name" + "middle_name" "last_name"
 #TODO: use string concatenation to build a custom message embedding the computed full_name
 
-# custom_message =
+# custom_message = "bonjour, "first_name" + "middle_name" "last_name"
+
+custom_message = "bonjour, " + full_name
 
 puts custom_message
